f0c98aabafef305535cc9604840bfc9d930fa159
tongtong <909061879@qq.com>
2018-10-10 21:47:44 +0800

    array

---
+++ array/src/array/LotteryDrawing.java
@@ -0,0 +1,33 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LotteryDrawing {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("how many number dou you need to draw?");
+		int k=in.nextInt();
+		
+		System.out.println("what is the highest numbe");
+		int n=in.nextInt();
+		
+		//fill an array with numbers 1 2 3....n
+		int[] numbers=new int[n];
+		for(int i=0;i<numbers.length;i++)
+			numbers[i]=i+1;
+		//draw k numbers and put them int a second array
+		int[] result=new int[k];
+		for(int i=0;i<result.length;i++) {
+			int r=(int)(Math.random()*n);
+			result[i]=numbers[r];
+			numbers[r]=numbers[n-1];
+			n--;
+		}
+		Arrays.sort(result);
+		System.out.println("Bet the following combination,it'll make you rich");
+		for(int r:result)
+			System.out.println(r);
+	}
+
+}
