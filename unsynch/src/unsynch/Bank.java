fd20e8e598bf5d32877bdb448029545a581a3d31
tongtong <909061879@qq.com>
2018-11-04 13:44:11 +0800

    线程同步

--- unsynch/src/unsynch/Bank.java
+++ unsynch/src/unsynch/Bank.java
@@ -10,10 +10,33 @@ import java.util.concurrent.locks.*;
  * 
  * 
  * 将transfer方法进行原子操作。把操作放在lock  unlock中
+ * 
+ * 常用API
+ * 线程优先级 void setPriority(int newPriority) MIN_PRIORITY,NORM_PRIORITY,MAX_PRIORITY
+ * static void yield()导致当前执行线程处于让步状态
+ * 守护线程  t.setDaemon(true);  
+ *      守护线程的作用是为其他线程提供服务，比如计时器
+ *     它不应该访问固有资源，如文件数据库等，因为它随时有可能中断
+*     public static boolean interrupted() 测试当前线程是否中断。 该方法可以清除线程的中断状态 。 
+*public boolean isInterrupted() 测试这个线程是否被中断。 线程的中断状态不受此方法的影响。
+
+ * 进程同步：
+ *   在竞争条件下，需要合理控制进程次序
+ *   锁对象：保证任何时刻只有一个线程进入临界区
+ *      lock...unlock
+ * void await（）  将该线程放到条件等待集中
+ * void signalAll() 接触该条件的等待集 中的所有线程阻塞状态
+ * void signal()  从该条件的等待集 中随机选取一个线程解除其阻塞状态。
+ *               该方法比signalAll更有效。但也有危险。有可能发生死锁
  */
 public class Bank
 {
    private final double[] accounts;
+   /*
+    * ReentrantLock implements the Lock interface,
+     *  每个 Bank对象有自己的ReeentrantLock对象,多个线程访问同一个Bank将串行工作，否则都不会发生阻塞
+     *  锁是可重入的，线程可以重复的获得已经持有的锁，锁通过持有计数器跟踪锁的嵌套调用
+    */
    private Lock bankLock;
    private Condition sufficientFunds;
 
@@ -35,6 +58,8 @@ public class Bank
     * @param from the account to transfer from
     * @param to the account to transfer to
     * @param amount the amount to transfer
+    * 
+    * 条件对象：
     */
    public void transfer(int from, int to, double amount) throws InterruptedException
    {
@@ -42,13 +67,13 @@ public class Bank
       try
       {
          while (accounts[from] < amount)
-            sufficientFunds.await();
+            sufficientFunds.await();//阻塞当前线程并放弃锁
          System.out.print(Thread.currentThread());
          accounts[from] -= amount;
          System.out.printf(" %10.2f from %d to %d", amount, from, to);
          accounts[to] += amount;
          System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
-         sufficientFunds.signalAll();
+         sufficientFunds.signalAll();//激活await的线程（接触阻塞状态）再次检测已满足条件的线程以运行
       }
       finally
       {
